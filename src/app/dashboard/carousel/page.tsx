4e33384f3244871e71f821d962247dd13dd17ea7
Carlos DIaz <c.diazadriann@gmail.com>
2024-07-16 16:46:44 -0400

    Agregando mas compoenntes

---
+++ src/app/dashboard/carousel/page.tsx
@@ -0,0 +1,72 @@
+"use client"
+import {
+  Card,
+  CardContent,
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components";
+//import Autoplay from "embla-carousel-autoplay"
+
+export default function CarouselPage() {
+  return (
+    <>
+    <div className="w-full justify-center flex">
+      <Carousel
+        className="w-full max-w-xs"
+        //plugins={[Autoplay({delay: 1000})]}
+        autoplay={1000}
+        opts={{
+          //dragFree:true
+          loop: true,
+        }}
+      >
+        <CarouselContent>
+          {Array.from({ length: 5 }).map((_, index) => (
+            <CarouselItem key={index} className="md:basis-1/2 lg:basis-full/3">
+              <div className="p-1">
+                <Card>
+                  <CardContent className="flex aspect-square items-center justify-center p-6">
+                    <span className="text-4xl font-semibold">{index + 1}</span>
+                  </CardContent>
+                </Card>
+              </div>
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+        <CarouselPrevious className="hidden sm:flex justify-center items-center" />
+        <CarouselNext className="hidden sm:flex justify-center items-center" />
+      </Carousel>
+
+      </div>
+      <div className="w-full justify-center flex">
+      <Carousel
+        className="w-full max-w-xs"
+        //plugins={[Autoplay({delay: 1000})]}
+        opts={{
+          //dragFree:true
+          loop: true,
+        }}
+      >
+        <CarouselContent>
+          {Array.from({ length: 5 }).map((_, index) => (
+            <CarouselItem key={index} className="md:basis-1/2 lg:basis-full/3">
+              <div className="p-1">
+                <Card>
+                  <CardContent className="flex aspect-square items-center justify-center p-6">
+                    <span className="text-4xl font-semibold">{index + 1}</span>
+                  </CardContent>
+                </Card>
+              </div>
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+        <CarouselPrevious className="hidden sm:flex justify-center items-center" />
+        <CarouselNext className="hidden sm:flex justify-center items-center" />
+      </Carousel>
+      </div>
+    </>
+  );
+}
--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -14,6 +14,13 @@ const links = [
   { name: "calendar", href: "calendar" },
   { name: "avatar", href: "avatar" },
   { name: "card", href: "card" },
+  { name: "carousel", href: "carousel" },
+  { name: "checkbox", href: "checkbox" },
+  { name: "command", href: "command" },
+  { name: "combobox", href: "combobox" },
+  { name: "context-menu", href: "context-menu" },
+  { name: "home", href: "home" },
+  { name: "menu-bar", href: "menu-bar" },
 ].sort((a, b) => a.name.localeCompare(b.name));
 
 export default function DashboardLayout({
@@ -155,7 +162,7 @@ export default function DashboardLayout({
               </div>
             </div>
           </main>
-          <footer className="bg-white md:flex md:items-center md:justify-between shadow rounded-lg p-4 md:p-6 xl:p-8 my-6 mx-4">
+          <footer className="bg-white md:flex md:items-center md:justify-between shadow rounded-lg p-4 md:p-6 xl:p-8 my-6 mx-4 print:hidden">
             <ul className="flex items-center flex-wrap mb-6 md:mb-0">
               <li>
                 <a
@@ -267,7 +274,7 @@ export default function DashboardLayout({
               </a>
             </div>
           </footer>
-          <p className="text-center text-sm text-gray-500 my-10">
+          <p className="text-center text-sm text-gray-500 my-10 print:hidden">
             &copy; 2019-{new Date().getFullYear()}{" "}
             <a href="#" className="hover:underline" target="_blank">
               Themesberg
